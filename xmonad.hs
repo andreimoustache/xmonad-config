18800956d3254c6d6ac06d32e3f2ede4261328e6
Andrei Mustata <andrei.mustata@gmail.com>
2013-02-27 01:28:45 +0000

    adding Solarized colour scheme

--- xmonad.hs
+++ xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Solarized
 
 main = do
     xmonad $ defaultConfig
@@ -12,6 +13,8 @@ main = do
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         , modMask = mod4Mask      -- rebind Mod to Winkey
         , handleEventHook = fullscreenEventHook
+        , normalBorderColor = solarizedBase01
+        , focusedBorderColor = solarizedRed
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock") -- Mod+Shift+L = lock screen
         ]
